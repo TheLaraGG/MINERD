035b9bfea15ed5e212350fd41bdbbce0cdf59ce1
TheLaraGG <Enmanuel.frias02@gmail.com>
2024-08-06 14:04:13 -0400

    Borrar incidencias

--- app/lib/screens/lista_incidencias.dart
+++ app/lib/screens/lista_incidencias.dart
@@ -22,11 +22,48 @@ class _ListaIncidenciasScreenState extends State<ListaIncidenciasScreen> {
     });
   }
 
+  Future<void> _deleteAllIncidencias() async {
+    await DatabaseHelper().deleteAllData(); // Cambiado a deleteAllData
+    setState(() {
+      incidencias.clear();
+    });
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Todos los registros han sido borrados.')),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Lista de Incidencias'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.delete_forever),
+            onPressed: () async {
+              final shouldDelete = await showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: Text('Borrar Todos los Registros'),
+                  content: Text('¿Estás seguro de que quieres borrar todos los registros?'),
+                  actions: [
+                    TextButton(
+                      onPressed: () => Navigator.of(context).pop(false),
+                      child: Text('Cancelar'),
+                    ),
+                    TextButton(
+                      onPressed: () => Navigator.of(context).pop(true),
+                      child: Text('Eliminar'),
+                    ),
+                  ],
+                ),
+              );
+              if (shouldDelete) {
+                await _deleteAllIncidencias();
+              }
+            },
+          ),
+        ],
       ),
       body: ListView.builder(
         itemCount: incidencias.length,
